dcba02163277b8491e1c3dc4bcb1e4a060f13129
liunian <dengjij@gmail.com>
2019-02-19 10:16:12 +0800

    优化图表

--- 2019/LPL/spring.js
+++ 2019/LPL/spring.js
@@ -1,8 +1,17 @@
 // project to: week, home, road, datetime, result
 const MATCHES = generateMatches(RAW_MATCH_DATA, TIME_ZONE);
+const WEEKS = MATCHES.map(i => i[0]).reduce((prev, cur) => prev.includes(cur) ? prev : prev.concat(cur), []).sort((a, b) => a - b);
+const latestWeek = getLatestWeek(MATCHES);
 
 const result = generateRankings(TEAMS, MATCHES);
-console.log(result);
+
+const $week = document.getElementById('J-week');
+const $weekVal = document.getElementById('J-week-val');
+const maxWeek = WEEKS[WEEKS.length - 1];
+
+$week.setAttribute('max', latestWeek);
+$week.value = latestWeek;
+$weekVal.innerText = latestWeek;
 
 const ds = new DataSet();
 const dv = ds.createView().source(result.rankings.slice(0).reverse());
@@ -15,8 +24,8 @@ dv.transform({
 
 const chart = new G2.Chart({
 	container: 'ranking',
-	width: 800,
-	// forceFit: true,
+	// width: 800,
+	forceFit: true,
 	height: 500
 });
 chart.scale({
@@ -33,5 +42,23 @@ chart.scale({
 });
 chart.source(dv);
 chart.coord().transpose();
-chart.intervalStack().position('team*point').color('winLoose', v => v === 'matchWin' ? '#2BBF30' : '#f5222d');
+chart.intervalStack().position('team*point')
+	.color('winLoose', v => v === 'matchWin' ? '#2BBF30' : '#f5222d')
+	.label('point*winLoose', (v, t) => {
+		if (v === 0) return false;
+		return {
+			position: 'middle',
+			offset: 0
+		};
+	});
 chart.render();
+
+function weekRangeHandler(evt) {
+	const curWeek = evt.target.value;
+	$weekVal.innerText = curWeek;
+	const curResult = generateRankings(TEAMS, MATCHES, curWeek);
+	dv.source(curResult.rankings.slice(0).reverse());
+}
+
+$week.addEventListener('change', weekRangeHandler, false);
+$week.addEventListener('input', weekRangeHandler, false);
